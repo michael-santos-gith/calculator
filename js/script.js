ff3d12064affb5f98c93a080121eee3e8d13b0a2
michael-santos-gith <maiconxandroid@gmail.com>
2022-04-13 12:05:28 -0300

    Modificado margin e font-size no style.css, adicionado IDs no index.html e pequenos ajustes, criado função de click no script.js.

---
+++ js/script.js
@@ -0,0 +1,54 @@
+let showNumber = document.querySelector('#show-number');
+let numbers = document.querySelectorAll('.numbers .numbers-click');
+
+numbers.forEach(function(val, ind) {
+    if (ind == 0) {
+        numbers[ind].addEventListener('click', function() {
+            showNumber.textContent = '';
+        })
+    } else if (ind == 1) {
+        numbers[ind].addEventListener('click', function() {
+            showNumber.textContent += 7;
+        })
+    } else if (ind == 2) {
+        numbers[ind].addEventListener('click', function() {
+            showNumber.textContent += 8;
+        })
+    } else if (ind == 3) {
+        numbers[ind].addEventListener('click', function() {
+            showNumber.textContent += 9;
+        })
+    } else if (ind == 4) {
+        numbers[ind].addEventListener('click', function() {
+            showNumber.textContent += 4;
+        })
+    } else if (ind == 5) {
+        numbers[ind].addEventListener('click', function() {
+            showNumber.textContent += 5;
+        })
+    } else if (ind == 6) {
+        numbers[ind].addEventListener('click', function() {
+            showNumber.textContent += 6;
+        })
+    } else if (ind == 7) {
+        numbers[ind].addEventListener('click', function() {
+            showNumber.textContent += 1;
+        })
+    } else if (ind == 8) {
+        numbers[ind].addEventListener('click', function() {
+            showNumber.textContent += 2;
+        })
+    } else if (ind == 9) {
+        numbers[ind].addEventListener('click', function() {
+            showNumber.textContent += 3;
+        })
+    } else if (ind == 10) {
+        numbers[ind].addEventListener('click', function() {
+            showNumber.textContent += 0;
+        })
+    } else if (ind == 11) {
+        numbers[ind].addEventListener('click', function() {
+            showNumber.textContent += '.';
+        })
+    }
+})
