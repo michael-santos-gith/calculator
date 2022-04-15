4c6d6e416e5039d5c0da1274a78b25acfe6be8f4
michael-santos-gith <maiconxandroid@gmail.com>
2022-04-15 20:08:59 -0300

    Aicionado a variavel que armazena os numeros para o calculo.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let showNumber = document.querySelector('#show-number');
 let numbers = document.querySelectorAll('.numbers .numbers-click');
 let symbolsOperation = document.querySelectorAll('.numbers .symbols-operation');
+let calculateNumbers = '';
+
 
 numbers.forEach(function(val, ind) {
     if (ind == 0) {
@@ -10,46 +12,57 @@ numbers.forEach(function(val, ind) {
     } else if (ind == 1) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 7;
+            calculateNumbers += 7;
         })
     } else if (ind == 2) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 8;
+            calculateNumbers += 8;
         })
     } else if (ind == 3) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 9;
+            calculateNumbers += 9;
         })
     } else if (ind == 4) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 4;
+            calculateNumbers += 4;
         })
     } else if (ind == 5) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 5;
+            calculateNumbers += 5;
         })
     } else if (ind == 6) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 6;
+            calculateNumbers += 6;
         })
     } else if (ind == 7) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 1;
+            calculateNumbers += 1;
         })
     } else if (ind == 8) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 2;
+            calculateNumbers += 2;
         })
     } else if (ind == 9) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 3;
+            calculateNumbers += 3;
         })
     } else if (ind == 10) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 0;
+            calculateNumbers += 0;
         })
     } else if (ind == 11) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += '.';
+            calculateNumbers += '.';
         })
     }
 })
