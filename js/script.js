d3bccd85008fc5f77a4ee79b0225b0cb2c1700c4
michael-santos-gith <maiconxandroid@gmail.com>
2022-04-15 21:15:15 -0300

    Adicionado o evento de clique nos simbolos dos botões.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ let numbers = document.querySelectorAll('.numbers .numbers-click');
 let symbolsOperation = document.querySelectorAll('.numbers .symbols-operation');
 let calculateNumbers = '';
 
-
 numbers.forEach(function(val, ind) {
     if (ind == 0) {
         numbers[ind].addEventListener('click', function() {
@@ -13,56 +12,118 @@ numbers.forEach(function(val, ind) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 7;
             calculateNumbers += 7;
+            console.log(calculateNumbers)
+
         })
     } else if (ind == 2) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 8;
             calculateNumbers += 8;
+            console.log(calculateNumbers)
+
         })
     } else if (ind == 3) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 9;
             calculateNumbers += 9;
+            console.log(calculateNumbers)
+
         })
     } else if (ind == 4) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 4;
             calculateNumbers += 4;
+            console.log(calculateNumbers)
+
         })
     } else if (ind == 5) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 5;
             calculateNumbers += 5;
+            console.log(calculateNumbers)
+
         })
     } else if (ind == 6) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 6;
             calculateNumbers += 6;
+            console.log(calculateNumbers)
+
         })
     } else if (ind == 7) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 1;
             calculateNumbers += 1;
+            console.log(calculateNumbers)
+
         })
     } else if (ind == 8) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 2;
             calculateNumbers += 2;
+            console.log(calculateNumbers)
+
         })
     } else if (ind == 9) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 3;
             calculateNumbers += 3;
+            console.log(calculateNumbers)
+
         })
     } else if (ind == 10) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += 0;
             calculateNumbers += 0;
+            console.log(calculateNumbers)
+
         })
     } else if (ind == 11) {
         numbers[ind].addEventListener('click', function() {
             showNumber.textContent += '.';
             calculateNumbers += '.';
+            console.log(calculateNumbers)
+
+        })
+    }
+})
+
+symbolsOperation.forEach(function(val, ind) {
+    if (ind == 0) {
+        symbolsOperation[ind].addEventListener('click', function() {
+            console.log('reset button')
+        })
+    } else if (ind == 1) {
+        symbolsOperation[ind].addEventListener('click', function() {
+            console.log('percent button')
+        })
+    } else if (ind == 2) {
+        symbolsOperation[ind].addEventListener('click', function() {
+            calculateNumbers += '/'
+            console.log(calculateNumbers)
+        })
+    } else if (ind == 3) {
+        symbolsOperation[ind].addEventListener('click', function() {
+            calculateNumbers += '*'
+            console.log(calculateNumbers)
+        })
+    } else if (ind == 4) {
+        symbolsOperation[ind].addEventListener('click', function() {
+            calculateNumbers += '-'
+            console.log(calculateNumbers)
+        })
+    } else if (ind == 5) {
+        symbolsOperation[ind].addEventListener('click', function() {
+            calculateNumbers += '+'
+            console.log(calculateNumbers)
+        })
+    } else if (ind == 6) {
+        symbolsOperation[ind].addEventListener('click', function() {
+            console.log('equals button')
+        })
+    } else if (ind == 7) {
+        symbolsOperation[ind].addEventListener('click', function() {
+            console.log('plus and minus button')
         })
     }
 })
